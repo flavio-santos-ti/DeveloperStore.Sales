256abd97054285d4794e8576b864fa7e6b137ba5
unknown <flavio.santos.ti@outlook.com>
2025-01-20 15:06:02 -0300

    feat:🆕 adicionar teste GET ID para Carrinhos.

--- tests/DeveloperStore.Sales.Tests/Services/ProductServiceTests.cs
+++ tests/DeveloperStore.Sales.Tests/Services/ProductServiceTests.cs
@@ -16,6 +16,7 @@ namespace DeveloperStore.Sales.Tests.Services
         private readonly IMapper _mapper;
         private readonly IProductService _productService;
 
+
         public ProductServiceTests()
         {
             _productRepository = Substitute.For<IProductRepository>();
