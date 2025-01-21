c704550b6141d45a2842866ad359d135881b1d12
unknown <flavio.santos.ti@outlook.com>
2025-01-21 14:51:40 -0300

    feat:🆕 Sale e SaleItem na camada Storage.

---
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Storage/Interfaces/ISaleItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeveloperStore.Sales.Domain.Models;
+
+namespace DeveloperStore.Sales.Storage.Interfaces;
+
+public interface ISaleItemRepository
+{
+    Task AddAsync(SaleItem saleItem);
+    Task<SaleItem?> GetByIdAsync(int id);
+    Task<IEnumerable<SaleItem>> GetBySaleIdAsync(int saleId);
+    Task UpdateAsync(SaleItem saleItem);
+    Task DeleteAsync(SaleItem saleItem);
+}
---
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Storage/Mappings/SaleItemMap.cs
@@ -0,0 +1,44 @@
+﻿using DeveloperStore.Sales.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeveloperStore.Sales.Storage.Mappings;
+
+[ExcludeFromCodeCoverage]
+public class SaleItemMap : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("sale_items");
+
+        builder.HasKey(si => si.Id);
+        builder.Property(si => si.Id).HasColumnName("id");
+
+        builder.Property(si => si.SaleId)
+               .HasColumnName("sale_id")
+               .IsRequired();
+
+        builder.Property(si => si.ProductId)
+               .HasColumnName("product_id")
+               .IsRequired();
+
+        builder.Property(si => si.Quantity)
+               .HasColumnName("quantity")
+               .IsRequired();
+
+        builder.Property(si => si.UnitPrice)
+               .HasColumnName("unit_price")
+               .HasColumnType("NUMERIC(10,2)")
+               .IsRequired();
+
+        builder.Property(si => si.Discount)
+               .HasColumnName("discount")
+               .HasColumnType("NUMERIC(10,2)");
+
+        builder.Property(si => si.TotalAmount)
+               .HasColumnName("total_amount")
+               .HasColumnType("NUMERIC(10,2)")
+               .IsRequired();
+    }
+}
