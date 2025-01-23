a309b066daa490b4b6757262cd0f30a17e98b914
unknown <flavio.santos.ti@outlook.com>
2025-01-23 17:21:17 -0300

    ♻️ref: Centralização de Add, Delete e Update de Carrinho na BaseRepository

--- src/DeveloperStore.Sales/DeveloperStore.Sales.Storage/Repositories/CartRepository.cs
+++ src/DeveloperStore.Sales/DeveloperStore.Sales.Storage/Repositories/CartRepository.cs
@@ -5,19 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DeveloperStore.Sales.Storage.Repositories;
 
-public class CartRepository : ICartRepository
+public class CartRepository : BaseRepository<Cart>, ICartRepository
 {
-    private readonly ApplicationDbContext _context;
+    public CartRepository(ApplicationDbContext context) : base(context) { }
 
-    public CartRepository(ApplicationDbContext context)
-    {
-        _context = context ?? throw new ArgumentNullException(nameof(context));
-    }
-
-    public async Task AddAsync(Cart cart)
-    {
-        await _context.Carts.AddAsync(cart);
-    }
 
     public async Task<Cart?> GetByIdAsync(int id)
     {
@@ -26,20 +17,8 @@ public class CartRepository : ICartRepository
             .FirstOrDefaultAsync(c => c.Id == id);
     }
 
-    public async Task DeleteAsync(Cart cart)
-    {
-        _context.Carts.Remove(cart);
-        await Task.CompletedTask;
-    }
-
     public IQueryable<Cart> GetAllQueryable()
     {
         return _context.Carts.Include(c => c.CartProducts); // Inclui os produtos do carrinho
-    }
-
-    public async Task UpdateAsync(Cart cart)
-    {
-        _context.Carts.Update(cart);
-        await Task.CompletedTask;
     }
 }
